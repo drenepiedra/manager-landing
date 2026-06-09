16081bdf2380a47924ece71fb181cba4b5657b35
Daniel Rene Piedra <daniel.piedra@get.tur.cu>
2026-06-09 16:28:14 -0400

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://drenepiedra.github.io/manager-landing/',
+  base: '/manager-landing',
 });
